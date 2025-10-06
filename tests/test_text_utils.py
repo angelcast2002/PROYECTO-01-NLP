d91efa1f15ea403ecbdc7d83846ac69523e66b86
Javier Alejandro Azurdia Arrecis <aleazurdia1@gmail.com>
2025-10-06 16:53:37 -0600

    Revert "Merge branch 'main' of https://github.com/angelcast2002/PROYECTO-01-NLP"
    This reverts commit 7f986aba8de55a63603dd8fd88b7a6cf49b598aa, reversing
    changes made to 43ee927abc398c25ba1c0cbaa1d6e84722186692.

--- tests/test_text_utils.py
+++
@@ -1,9 +0,0 @@
-from pipeline import normalize, tokenize_simple, clean_tokens
-
-def test_normalize():
-    assert normalize("Árbol\n NUEVO\t") == "arbol nuevo"
-
-def test_tokenize_and_clean():
-    toks = clean_tokens(tokenize_simple("hola, 2024 mundo y el la de"), remove_digits=True, remove_sw=True)
-    assert "hola" in toks and "mundo" in toks
-    assert "2024" not in toks
